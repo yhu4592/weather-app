18719e75dbb52b3f20a38a76955265a617d44d66
Yongkang Hu <hu.yongkangsg114@gmail.com>
2021-12-15 22:43:10 -0600

    add search to fetch weather from input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+import axios from 'axios'
 
 const App = () => {
+  const [weather, setWeather] = useState()
+  const [city, setCity] = useState('')
+
+  const handleChange = (event) => {
+    setCity(event.target.value)
+  }
+
+  const handleClick = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/forecast/daily?city=${city}&key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => setWeather(response.data.data))
+  }
+
   return (
-    <div>Hi</div>
+    <div>
+      <input
+        onChange={handleChange}
+        type='text'
+        value={city}
+      />
+      <button onClick={handleClick}>Search</button>
+    </div>
   )
 }
 
