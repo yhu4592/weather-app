185576d586ad8e9e46251d7678bea09da1bfa193
Yongkang Hu <hu.yongkangsg114@gmail.com>
2021-12-18 00:42:15 -0600

    feat: add state convert weather to imperial/metric units

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CurrentWeather from './CurrentWeather'
 import ForecastWeather from './ForecastWeather'
 
 const Weather = ({weather}) => {
+  const [isImperial, setIsImperial] = useState(true)
   let currentWeather = weather[0]
   let forecastWeather = weather.slice(1)
 
+  const handleClick = () => {
+    setIsImperial(!isImperial)
+  }
+
   return (
     <div id='weather'>
-      <CurrentWeather currentWeather={currentWeather}/>
-      <ForecastWeather forecasts={forecastWeather}/>
+      <CurrentWeather 
+        isImperial={isImperial} 
+        currentWeather={currentWeather}
+      />
+      <button onClick={handleClick}>F</button>
+      <button onClick={handleClick}>C</button>
+      <ForecastWeather
+        isImperial={isImperial} 
+        forecasts={forecastWeather}
+      />
     </div>
   )
 }
