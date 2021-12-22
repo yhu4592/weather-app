c5845cfe9a035e85cdb6c7ed9f3dcee8e9f77822
Yongkang Hu <hu.yongkangsg114@gmail.com>
2021-12-21 22:39:11 -0600

    feat: add unit conversion conditonal rendering

--- src/components/weather/ForecastDay.js
+++ src/components/weather/ForecastDay.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import metric from '../../modules/toMetric'
 
-const ForecastDay = ({forecast}) => {
+const ForecastDay = ({forecast, isImperial}) => {
   return (
     <div id='forecastDay'>
-      {console.log(forecast)}
+      <p><p>Date: {forecast.datetime}</p></p>
+      <p>Weather: {forecast.weather.description}</p>
+      <p>Temp: {isImperial 
+                 ? forecast.temp 
+                 : metric.toCelcius(forecast.temp)
+               }
+      </p>
     </div>
   )
 }
