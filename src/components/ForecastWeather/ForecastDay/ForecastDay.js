8bafd2eb27f5b8d5de1dd2ab849c48bb9642baab
Yongkang Hu <hu.yongkangsg114@gmail.com>
2021-12-26 19:00:48 -0600

    feat: add precipitation chance

--- src/components/ForecastWeather/ForecastDay/ForecastDay.js
+++ src/components/ForecastWeather/ForecastDay/ForecastDay.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import toMetric from '../../../modules/toMetric'
+import './ForecastDay.css'
 
 const ForecastDay = ({forecast, isImperial}) => {
   return (
     <div id='forecastDay'>
       <p>Date: {forecast.datetime}</p>
       <p>Weather: {forecast.weather.description}</p>
+      <p className='precip'>{(forecast.precip*100).toFixed()}%</p>
       <img 
         src={`/images/${forecast.weather.icon}.png`}
         alt={`${forecast.weather.icon} + image.`}
